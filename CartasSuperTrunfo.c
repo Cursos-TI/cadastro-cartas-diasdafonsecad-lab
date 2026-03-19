e75636749930113534eadeb9c16cacd678b79d00
diasdafonsecad-lab <diasdafonsecad@gmail.com>
2026-03-19 17:15:02 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,11 +5,40 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
 
-  // Área para entrada de dados
+//variáveis da carta 1
+char estado1, código1[10], cidade1[20];
+int população1, ponto_turistico1;
+float área1, pib1;
 
-  // Área para exibição dos dados da cidade
+//variáveis da carta 2
+char estado2, código2[10], cidade2[20];
+int população2, ponto_turistico2;
+float área2, pib2;
+
+
+
+//entrada de dados da carta 1 
+printf("Digite o estado:"); ("digite o código:"); ("digite a cidade:")
+printf("digite a população:"); ("digite o ponto_turístico:");
+printf("digite a área:"); ("digite o pib:");
+
+//entrada de dados da carta 2 
+printf("Digite o estado:"); ("digite o código:"); ("digite a cidade:")
+printf("digite a população:"); ("digite o ponto_turístico:");
+printf("digite a área:"); ("digite o pib:");
+
+
+
+  // Área para exibição dos dados da carta 1
+scanf("%c", &estado A ); scanf("%c" , &código A01); scanf("%c" ,&cidade São Paulo)
+scanf("%d" , &população 12325000); scanf("%d" , &ponto_turistico 50);
+scanf("%f" , &área 1521.11km²); scanf("%f" , &pib 699.28 bilhões de reais);
+
+//área para exibição dos dados da carta 2 
+scanf("%c", &estado B); scanf("%c" , &código B02); scanf("%c" ,&cidade Rio de Janeiro)
+scanf("%d" , &população 6748000); scanf("%d" , &ponto_turistico 30);
+scanf("%f" , &área 1200.25km²); scanf("%f" , &pib 300.bilhões de reais);
 
 return 0;
 } 
